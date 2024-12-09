9d10d06eca1807d6f06ec04670676f492280536c
SuguiuraFelipe <felipesuguiura.pro@gmail.com>
2024-12-09 17:56:41 +0000

    Criação de middleware para verificar a ordem de execução e seus tipos, finalizando com o tratamento de erro

--- 06-middleware/src/index.js
+++ 06-middleware/src/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const middlewareC = require('./middleware/middleware-c')
 
 const app = express()
+
+app.use(middlewareC)
 
 app.use(function(req, res, next){
     console.log('Executando middleware A')
@@ -16,12 +19,23 @@ function middlewareB(req, res, next){
 
 app.get('/testeA', (req, res) =>{
     console.log({a: req.middlewareA, b: req.middlewareB})
+    throw new Error('Deu ruim')
     res.end()
 })
 
 app.get('/testeB', middlewareB, (req, res) =>{
     console.log({a: req.middlewareA ,b: req.middlewareB})
     res.end()
+})
+
+app.use(function(err, req, res, next){
+    if(err){
+        console.log(err.message)
+        res.status(400)
+        res.json({message: err.message})
+    }else{
+        next()
+    }
 })
 
 const PORT = 3000
